10d65575a052a515a49fb6c1528606745890c814
Roberto Ughi <umbe77@gmail.com>
2022-02-21 18:48:22 +0100

    Fix Org Mode

--- nvim/.config/nvim/lua/umbe/orgmode.lua
+++ nvim/.config/nvim/lua/umbe/orgmode.lua
@@ -1,23 +1,24 @@
-require"orgmode".setup{
-	org_agenda_files = {'~/my-orgs/**/*'},
-	org_default_notes_file = '~/my-orgs/refile.org',
+require("orgmode").setup_ts_grammar()
+require("orgmode").setup({
+	org_agenda_files = { "~/my-orgs/**/*" },
+	org_default_notes_file = "~/my-orgs/refile.org",
 	mappings = {
 		org = {
-			org_insert_todo_heading = 'oit',
-			org_time_stamp = '<Leader>oq',
-			org_todo = '<Leader>od',
-			org_open_at_point = '<Leader>ow',
-			org_do_promote = '<Leader>,',
-			org_do_demote = '<Leader>.',
-			org_promote_subtree = '<Leader>,q',
-			org_demote_subtree = '<Leader>.q',
-			org_show_help = '?',
+			org_insert_todo_heading = "oit",
+			org_time_stamp = "<Leader>oq",
+			org_todo = "<Leader>od",
+			org_open_at_point = "<Leader>ow",
+			org_do_promote = "<Leader>,",
+			org_do_demote = "<Leader>.",
+			org_promote_subtree = "<Leader>,q",
+			org_demote_subtree = "<Leader>.q",
+			org_show_help = "?",
 		},
 	},
-}
+})
 
-require"org-bullets".setup{
-    symbols = { "◉", "○", "✸", "✿" }
-}
+require("org-bullets").setup({
+	symbols = { "◉", "○", "✸", "✿" },
+})
 
- -- require"headlines".setup{}
+-- require"headlines".setup{}
