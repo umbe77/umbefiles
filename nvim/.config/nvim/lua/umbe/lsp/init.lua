8c985d18346c0fbec3eb9c283da8850336f3eddc
Roberto Ughi <umbe77@gmail.com>
2022-03-22 21:13:52 +0100

    Add json lsp server

--- nvim/.config/nvim/lua/umbe/lsp/init.lua
+++ nvim/.config/nvim/lua/umbe/lsp/init.lua
@@ -1,18 +1,27 @@
-vim.fn.sign_define("LspDiagnosticsSignError",
-                   {texthl = "LspDiagnosticsSignError", text = "", numhl = "LspDiagnosticsSignError"})
-vim.fn.sign_define("LspDiagnosticsSignWarning",
-                   {texthl = "LspDiagnosticsSignWarning", text = "", numhl = "LspDiagnosticsSignWarning"})
-vim.fn.sign_define("LspDiagnosticsSignHint",
-                   {texthl = "LspDiagnosticsSignHint", text = "", numhl = "LspDiagnosticsSignHint"})
-vim.fn.sign_define("LspDiagnosticsSignInformation",
-                   {texthl = "LspDiagnosticsSignInformation", text = "", numhl = "LspDiagnosticsSignInformation"})
+vim.fn.sign_define(
+	"LspDiagnosticsSignError",
+	{ texthl = "LspDiagnosticsSignError", text = "", numhl = "LspDiagnosticsSignError" }
+)
+vim.fn.sign_define(
+	"LspDiagnosticsSignWarning",
+	{ texthl = "LspDiagnosticsSignWarning", text = "", numhl = "LspDiagnosticsSignWarning" }
+)
+vim.fn.sign_define(
+	"LspDiagnosticsSignHint",
+	{ texthl = "LspDiagnosticsSignHint", text = "", numhl = "LspDiagnosticsSignHint" }
+)
+vim.fn.sign_define(
+	"LspDiagnosticsSignInformation",
+	{ texthl = "LspDiagnosticsSignInformation", text = "", numhl = "LspDiagnosticsSignInformation" }
+)
 
-require "umbe.lsp.sumneko_lua"
-require "umbe.lsp.go"
-require "umbe.lsp.tsserver"
-require "umbe.lsp.omnisharp"
-require "umbe.lsp.bash"
-require "umbe.lsp.svelte"
-require "umbe.lsp.docker"
-require "umbe.lsp.tailwindcss"
-require "umbe.lsp.nullls"
+require("umbe.lsp.sumneko_lua")
+require("umbe.lsp.go")
+require("umbe.lsp.tsserver")
+require("umbe.lsp.omnisharp")
+require("umbe.lsp.bash")
+require("umbe.lsp.svelte")
+require("umbe.lsp.docker")
+require("umbe.lsp.tailwindcss")
+require("umbe.lsp.nullls")
+require("umbe.lsp.json")
---
+++ nvim/.config/nvim/lua/umbe/lsp/json.lua
@@ -0,0 +1,10 @@
+local data_path = vim.fn.stdpath("data")
+require("lspconfig").jsonls.setup({
+	cmd = {
+		data_path .. "/lsp_servers/jsonls/node_modules/vscode-langservers-extracted/bin/vscode-json-language-server",
+	},
+	capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+	on_attach = function() -- possible values on thid function (client, bufnr)
+		require("lsp_signature").on_attach()
+	end,
+})
